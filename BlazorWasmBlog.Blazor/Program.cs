577a5c9135c24f8f223e47803da878557a01a073
W.Paap <wpaap@hotmail.com>
2020-02-07 12:05:38 +0100

    Configures injected services for blazor-ui project: squidex-cms configuration demo step

--- BlazorWasmBlog.Blazor/Program.cs
+++ BlazorWasmBlog.Blazor/Program.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
+﻿#pragma warning disable RCS1102 // Make class static.
+
+using BlazorWasmBlog.Core.Application;
+using BlazorWasmBlog.Modules.SquidexCms;
 using Microsoft.AspNetCore.Blazor.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
 
 namespace BlazorWasmBlog.Blazor
 {
@@ -14,7 +15,21 @@ namespace BlazorWasmBlog.Blazor
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            RegisterServices(builder.Services);
+
             await builder.Build().RunAsync();
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            // Configuration
+            services.AddDefaultConfigSettingStore(); // use default in-memory configuration
+            services.AddSquidexCms(); // using squidex as Content Management System
+
+            // TODO: Future service registrations here: ...
+            // Caching
+            // Http and retry policies
+            // Blog-post services
         }
     }
 }
