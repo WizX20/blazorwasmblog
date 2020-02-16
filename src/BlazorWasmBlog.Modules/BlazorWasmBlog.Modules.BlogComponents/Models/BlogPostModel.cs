bfecd79843daf56f4365546d48624c9d680a7bc0
W.Paap <wpaap@hotmail.com>
2020-02-16 15:03:29 +0100

    Adds new blog-posts component library and changes the fetchdata example to demo the blog-posts component

---
+++ src/BlazorWasmBlog.Modules/BlazorWasmBlog.Modules.BlogComponents/Models/BlogPostModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BlazorWasmBlog.Modules.BlogComponents.Models
+{
+    public class BlogPostModel
+    {
+        public string Id { get; set; }
+
+        public string Slug { get; set; }
+
+        public string Title { get; set; }
+
+        public string IntroductionText { get; set; }
+
+        public string Text { get; set; }
+
+        public int ReadingMinutes { get; set; }
+
+        public string TeaserImage { get; set; }
+
+        public string MainImage { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
